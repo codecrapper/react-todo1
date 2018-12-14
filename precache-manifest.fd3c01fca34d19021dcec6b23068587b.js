a554d16c07fe7540cd585cb21117f61b878f2e0b
John Nguyen <john.code9592@gmail.com>
2018-12-14 17:21:59 +1100

    Updates

---
+++ precache-manifest.fd3c01fca34d19021dcec6b23068587b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f1125e1f21db0fcac51",
+    "url": "/react-todo1/static/js/runtime~main.2f1125e1.js"
+  },
+  {
+    "revision": "1e0686fd9e0d23de3c89",
+    "url": "/react-todo1/static/js/main.1e0686fd.chunk.js"
+  },
+  {
+    "revision": "20bc491bae5e667480bb",
+    "url": "/react-todo1/static/js/1.20bc491b.chunk.js"
+  },
+  {
+    "revision": "1e0686fd9e0d23de3c89",
+    "url": "/react-todo1/static/css/main.e0d2128c.chunk.css"
+  },
+  {
+    "revision": "1c84b4312db6ed2daac72ec6d72ed4e1",
+    "url": "/react-todo1/index.html"
+  }
+];
